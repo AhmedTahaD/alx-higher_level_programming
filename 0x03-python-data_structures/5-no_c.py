c015670d973b0aed6fd3479530b62e8d6358c9aa
AhmedTahaD <ahmedtaha2893@gmail.com>
2024-01-07 20:50:30 +0200

    remove c from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     letters = list(my_string)
-    
     for letter in letters:
         if letter == 'c' or letter == 'C':
             letters.remove(letter)
