438d3a4795a8b2cc028ca53b768f80ecd8345101
AhmedTahaD <ahmedtaha2893@gmail.com>
2024-01-28 21:26:22 +0200

    add tow integer

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Function to add two integer"""
+
+
+def add_integer(a, b=98):
+    """a: firstintegers
+    b:second integer
+    raise a TypeError exception
+    if a, b are not int or float
+    Returns an integer: the addition of two integers.
+    """
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
