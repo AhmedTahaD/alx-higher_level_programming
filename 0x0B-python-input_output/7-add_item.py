d15b790d99d09ba7bf7aba150245d65dbf77101b
AhmedTahaD <ahmedtaha2893@gmail.com>
2024-02-06 23:59:38 +0200

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,11 +2,11 @@
 """
 script to save and load
 """
-from sys import argv
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item,json"
+filename = 'add_item,json'
 arglist = list(sys.argv[1:])
 
 try:
