bd055ccf243901f9fe7550f966451058af282ea4
AhmedTahaD <ahmedtaha2893@gmail.com>
2024-02-07 00:03:05 +0200

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,13 +6,12 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = 'add_item,json'
 arglist = list(sys.argv[1:])
 
 try:
-    data = load_from_json_file(filename)
+    data = load_from_json_file('add_item,json')
 except Exception:
     data = []
 
 data.extend(arglist)
-save_to_json_file(data, filename)
+save_to_json_file(data, 'add_item,json')
