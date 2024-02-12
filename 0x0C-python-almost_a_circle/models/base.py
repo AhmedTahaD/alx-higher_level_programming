ba2aa2b0969bdd6777bef77d914ddc9a48b14fce
AhmedTahaD <ahmedtaha2893@gmail.com>
2024-02-12 21:11:19 +0200

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,8 +2,6 @@
 """This class will be the “base” of all other classes in this project.
 The goal of it is to manage id attribute in all your future classes
 and to avoid duplicating the same code (by extension, same bugs)"""
-from json import dumps, loads
-import csv
 
 
 class Base:
