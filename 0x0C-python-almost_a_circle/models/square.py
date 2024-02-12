f29b3ad691ed8206faa70b1c290049ca8cd3134e
AhmedTahaD <ahmedtaha2893@gmail.com>
2024-02-12 22:43:32 +0200

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,13 +4,23 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''A Square class.'''
+    """A Square class."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        '''Constructor.'''
+        """Square initiation"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        '''Returns string info about this square.'''
+        """Returns string info about this square."""
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """Size of this square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
